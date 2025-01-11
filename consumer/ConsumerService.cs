ab7165e885d2de9aeaef4ecb889a4f405b54f253
Netto <rodrigomnto@gmail.com>
2025-01-11 11:47:15 -0300

    project created

---
+++ consumer/ConsumerService.cs
@@ -0,0 +1,15 @@
+
+
+
+public class ConsumerService : IHostedService
+{
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
